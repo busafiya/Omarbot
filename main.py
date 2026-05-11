20d6700871e7479ff4e51aacd638754bef1d150a
busafiya <omar187137@gmail.com>
2026-05-11 15:31:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("omar alpha bot running")
+import os
+
+print("omar alpha bot running")
+
+api = os.getenv("BINANCE_API_KEY")
+secret = os.getenv("BINANCE_SECRET_KEY")
+
+print("API Loaded:", bool(api))
+print("SECRET Loaded:", bool(secret))
